841160aec6bd4790e4e1f26d30d1cc8b081ad6f7
stevenAnto <ecalcinap@unsa.edu.pe>
2022-06-11 15:41:19 -0500

    se implemento segundo ejercicio y se probo los casos de la guia

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -1,21 +1,39 @@
-package com.example.project;
+//package com.example.project;
 
 import java.util.Scanner;
 
 public class Exercise2 {
-    public static void main(String[] args) {
-        Exercise2 obj = new Exercise2();
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            String s = sc.nextLine();
-            System.out.println(obj.existenDuplicados(s));
-        }
+  public static void main(String[] args) {
+    Exercise2 obj = new Exercise2();
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      String s = sc.nextLine();
+      System.out.println(obj.existenDuplicados(s));
     }
+  }
 
-    public boolean existenDuplicados(String str) {
-        MyStack<Character> stack = new LinkedListStack<>();
-        // Colocar codigo aqui
-
-        return false;
+  public boolean existenDuplicados(String str) {
+    MyStack<Character> stack = new LinkedListStack<>();
+    for (int i = 0; i<str.length();i++){
+      Character a = new Character('(');
+      if(str.charAt(i)!= ')'){
+	stack.push(str.charAt(i));
+	System.out.println(stack);
+      }
+      else{
+	int contador = 0;
+	while (!(stack.top().equals(a))){
+	  stack.pop();
+	  contador++;
+	}
+	stack.pop();//quito el parentesis de aperturar
+	contador++;
+	System.out.println("contador "+ contador);
+	System.out.println(stack);
+	if(contador==1)return true;
+      }
     }
+    // Colocar codigo aqui
+    return false;
+  }
 }
